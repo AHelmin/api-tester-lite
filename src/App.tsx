bdc6e7ba622ab044c032bfd5e37758d54923e284
helm0127 <helm0127@morris.umn.edu>
2025-09-22 16:12:59 -0500

    start of app directory restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import { Container, Navbar, Nav, Card, Button, Form, Row, Col, DropdownMenu } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/index'
 import './App.css'
 
 export default function App() {
+
+
+
   return (
     <>
-      <Navbar expand="md" className='' bg='dark' variant='dark'>
+      <BrowserRouter>
+        <Navbar expand="md" className='' bg='dark' variant='dark'>
         <Container>
           <Navbar.Brand href="#home">API Tester Lite</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -21,30 +27,11 @@ export default function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Card>
-        <Container>
-          <Card.Body>
-            <Card.Title>Request</Card.Title>
-            <Form>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Request Type</Form.Label>
-                <Form.Control type="email" placeholder="Please enter request url" />
-              </Form.Group>
-              <Form.Select aria-label="Default select example">
-                <option>Please select HTTP method</option>
-                <option value="1">GET</option>
-                <option value="2">POST</option>
-                <option value="3">PUT</option>
-                <option value="4">DELETE</option>
-              </Form.Select>
-              <div className="d-flex gap-2 mt-3">
-                <Button variant="primary">Send</Button>
-                <Button variant="outline-secondary">Clear</Button>
-              </div>
-            </Form>
-          </Card.Body>
-        </Container>
-      </Card>
+
+      <Routes>
+      <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter >
 
     </>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+
+import { Container, Card, Button, Form } from 'react-bootstrap';
+
+export default function Home() {
+    
+    function onSendClick(e: React.MouseEvent<HTMLFormElement>) {
+        e.preventDefault();
+    
+      }
+    
+      function onClearClick(e: React.MouseEvent<HTMLFormElement>) {
+        e.preventDefault();
+      }
+
+return( 
+<Card>
+        <Container>
+          <Card.Body>
+            <Card.Title>Request</Card.Title>
+            <Form>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control type="email" placeholder="Please enter request url" />
+              </Form.Group>
+              <Form.Label>Request Type</Form.Label>
+              <Form.Select aria-label="Default select example">
+                <option>Please select HTTP method</option>
+                <option value="1">GET</option>
+                <option value="2">POST</option>
+                <option value="3">PUT</option>
+                <option value="4">DELETE</option>
+              </Form.Select>
+              <div className="d-flex gap-2 mt-3">
+                <Button variant="primary" onClick={onSendClick}>Send</Button>
+                <Button variant="outline-secondary" onClick={onClearClick}>Clear</Button>
+              </div>
+            </Form>
+          </Card.Body>
+        </Container>
+      </Card>
+      )
+    }
