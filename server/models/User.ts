9be5f001cef929d34dc19b9a73e90fbceef682c2
helm0127 <helm0127@morris.umn.edu>
2025-09-24 17:17:36 -0500

    began working backend, added db config file and User model

---
+++ server/models/User.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+export interface IUser extends mongoose.Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required.'],
+        trim: true,
+        lowercase: true,
+        match: [/^[a-z0-9._-]+@[\da-z.-]+\.[a-z.]{2,6}$/i, 'Email is invalid.'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 128,
+        trim: true,
+        select: false,
+    },
+    
+}, {
+    timestamps: true,
+}
+)
+
+const User = mongoose.model<IUser>('User', userSchema)
+export default User;
