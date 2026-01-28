ddefc721b0ac41dd4ca76d2569d55798f4c08369
helm0127 <helm0127@morris.umn.edu>
2026-01-28 14:00:38 -0600

    wrapped request form with a <form>, added send button, created simple test function for a GET response

--- src/components/RequestForm.jsx
+++ src/components/RequestForm.jsx
@@ -4,106 +4,122 @@ export default function RequestForm() {
   const [open, setOpen] = useState(false);
   const [method, setMethod] = useState("GET");
   const [url, setUrl] = useState("");
+  const [results, setResults] = useState("");
 
   function handleUrlChange(e) {
     setUrl(e.target.value);
+  }
+
+  async function handleSend(e) {
+    e.preventDefault();
+    const res = await fetch(url);
+    const data = await res.json();
+    setResults(data);
   }
 
   return (
     <>
       <div className="card">
         <h2>Request</h2>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <h4>Please choose method</h4>
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                onClick={() => setOpen((o) => !o)}
-                aria-expanded={open}
-              >
-                {method}
-              </button>
-              <ul className={`dropdown-menu ${open ? "show" : ""}`}>
-                <li>
-                  <button
-                    className="dropdown-item"
-                    onClick={() => {
-                      setMethod("GET");
-                      setOpen(false);
-                    }}
-                  >
-                    GET
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="dropdown-item"
-                    onClick={() => {
-                      setMethod("POST");
-                      setOpen(false);
-                    }}
-                  >
-                    POST
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="dropdown-item"
-                    onClick={() => {
-                      setMethod("PUT");
-                      setOpen(false);
-                    }}
-                  >
-                    PUT
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="dropdown-item"
-                    onClick={() => {
-                      setMethod("DELETE");
-                      setOpen(false);
-                    }}
-                  >
-                    DELETE
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li className="list-group-item">
-            <label htmlFor="url">Please enter Request URL: </label>
-            <input
-              type="url"
-              name="url"
-              className="form-control"
-              id="url"
-              placeholder=""
-              onChange={handleUrlChange}
-            ></input>
-          </li>
-          {(method === "PUT" || method === "POST") && (
+        <form onSubmit={handleSend}>
+          <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              Request Body
-              <div className="form-floating">
-                <textarea
-                  className="form-control"
-                  placeholder="Leave a comment here"
-                  id="request-body"
-                ></textarea>
-                <label htmlFor="request-body">Enter request body</label>
+              <h4>Please choose method</h4>
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  onClick={() => setOpen((o) => !o)}
+                  aria-expanded={open}
+                >
+                  {method}
+                </button>
+                <ul className={`dropdown-menu ${open ? "show" : ""}`}>
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => {
+                        setMethod("GET");
+                        setOpen(false);
+                      }}
+                    >
+                      GET
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => {
+                        setMethod("POST");
+                        setOpen(false);
+                      }}
+                    >
+                      POST
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => {
+                        setMethod("PUT");
+                        setOpen(false);
+                      }}
+                    >
+                      PUT
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => {
+                        setMethod("DELETE");
+                        setOpen(false);
+                      }}
+                    >
+                      DELETE
+                    </button>
+                  </li>
+                </ul>
               </div>
             </li>
-          )}
-        </ul>
+            <li className="list-group-item">
+              <label htmlFor="url">Please enter Request URL: </label>
+              <input
+                type="url"
+                name="url"
+                className="form-control"
+                id="url"
+                placeholder=""
+                onChange={handleUrlChange}
+              ></input>
+            </li>
+            {(method === "PUT" || method === "POST") && (
+              <li className="list-group-item">
+                Request Body
+                <div className="form-floating">
+                  <textarea
+                    className="form-control"
+                    placeholder="Leave a comment here"
+                    id="request-body"
+                  ></textarea>
+                  <label htmlFor="request-body">Enter request body</label>
+                </div>
+              </li>
+            )}
+            <li className="list-group-item">
+              <button type="submit" className="btn btn-primary">
+                Send
+              </button>
+            </li>
+          </ul>
+        </form>
         <pre>{method}</pre>
       </div>
       <div className="row container-fluid">
         <div className="col-12 col-lg-7 border border-light">
           <div className="row border border-light">
             <h2>Results</h2>
+            {results && <pre>{JSON.stringify(results)}</pre>}
           </div>
         </div>
         <div className="col-12 col-lg-5 border border-light">
