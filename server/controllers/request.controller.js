7575bbfed47ee3bebef8231ca02ed00df0bf07cd
helm0127 <helm0127@morris.umn.edu>
2026-02-09 17:03:14 -0600

    installed bcrypt, began to add user controller

--- server/controllers/request.controller.js
+++ server/controllers/request.controller.js
@@ -2,4 +2,7 @@ import Request from '../models/Request.js'
   
   
 
+ //TODO: POST new request(tagged with user info)
+ //TODO: GET history(limit sotred history to 10)
+ //TODO: DELETE item/ or delete all?
  
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,1 +1,16 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+//TODO: need sign up controller
+export async function createUser(data) {
+  const hash = await bcrypt.hash(data.password, 10);
+  const userData = { ...data, password: hash };
+  try {
+    return await User.create(userData);
+  } catch (err) {
+    console.log(err.message);
+    throw new Error(err);
+  }
+};
+//TODO: need login controller
+//TODO: logout controller(likely will need to be handled here since I will be checking loggedIn from backend)
