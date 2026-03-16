a4d2ad2a836e9c0bd9d8ce10b2ff7edbdae501fd
helm0127 <helm0127@morris.umn.edu>
2026-03-16 14:59:58 -0500

    began JWT and cookie implementation to be used as auth method

--- server/routes/api/user.routes.js
+++ server/routes/api/user.routes.js
@@ -1,26 +1,50 @@
-import express from 'express'
-import { createUser, handleLogin } from '../../controllers/user.controller.js'
+import express from "express";
+import { createUser, handleLogin } from "../../controllers/user.controller.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-    try {
-      const payload = await createUser(req.body)
-      res.status(200).json({ status: "success", payload })
-    }catch(err){
-      console.log(err.message)
-      res.status(500).json({ status: "error", payload: err.message })
-    }
-  });
-  
-  router.post("/login", async(req, res) => {
-    try {
-      const payload = await handleLogin(req.body.email, req.body.password)
-      res.status(200).json({ status: "success", payload })
-    }catch(err){
-      console.log(err.message)
-      res.status(401).json({ status: "error", payload: "Could not authorize." })
-    }
-  });
+// tokens are encrypted non-invasive data about the user
+function createToken(id) {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET);
+}
 
-  export default router;
+router.post("/", async (req, res) => {
+  try {
+    const payload = await createUser(req.body);
+    const token = createToken(payload._id);
+    res
+      .status(200)
+      .cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600000,
+        sameSite: 'lax'
+      })
+      .json({ status: "success", payload });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ status: "error", payload: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const payload = await handleLogin(req.body.email, req.body.password);
+    const token = createToken(payload._id);
+    res
+      .status(200)
+      .cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600000,
+        sameSite: 'lax'
+      })
+      .json({ status: "success", payload });
+  } catch (err) {
+    console.log(err.message);
+    res.status(401).json({ status: "error", payload: "Could not authorize." });
+  }
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from 'cors';
 import routes from './routes/index.js'
 import { connectDB } from "./config/connection.js";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(routes);
 
