4afc8bcb10abca49c5a64c696e07108381782b64
helm0127 <helm0127@morris.umn.edu>
2026-05-26 14:46:14 -0500

    fixed typo preventing server from running

--- server/routes/api/user.routes.js
+++ server/routes/api/user.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createUser, handleLogin } from "../../controllers/user.controller.js";
 import jwt from "jsonwebtoken";
-import User from '../../models/User.js'
+import { User } from '../../models/User.js'
 import { requireAuth } from "../../middleware/requireAuth.js";
 
 const router = express.Router();
